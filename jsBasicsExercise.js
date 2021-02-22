dc2f1a6b6ecf0578fb919f639252099564619482
Andrew Bertram <andrew.bertram23@gmail.com>
2021-02-22 13:00:17 -0500

    Completed exercise as well as bonus section.

---
+++ jsBasicsExercise.js
@@ -0,0 +1,56 @@
+// 1
+console.log(18);
+
+// 2
+// Numbers, Strings, Booleans, Null, and Undefined
+
+// 3
+// +, -, *, /, **, %
+
+// 4
+6%5;
+// console.log(6%5);
+5%4;
+// console.log(5%4);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+// 6
+"Wanda" + "Vision";
+// console.log("Wanda" + "Vision");
+
+// 7
+"Andrew".length + "Bertram".length;
+// console.log("Andrew".length + "Bertram".length);
+
+// 8
+"Avengers Endgame"[15];
+// console.log("Avengers Endgame"[15]);
+
+// BONUS
+
+// 1
+console.log("\\");
+
+// 2
+console.log(5**4-9);
+
+// 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+var m = 10;
+var c = 5;
+var u = m + c;
+// console.log(u);
